8bc2d4025c6da871689cc1d1aaa08513fec4c333
Leonardo Galante <lnd@MacBook-Pro-de-Leonardo.local>
2016-09-28 15:54:14 -0300

    meetupJS friendship!

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var imagemin = require('gulp-imagemin');
 var htmlmin = require('gulp-htmlmin');
 
 
-gulp.task('uglify', function() {
+gulp.task('uglify', function () {
     return gulp.src('./src/js/script.js')
         .pipe(uglify())
         .pipe(rename('main.min.js'))
         .pipe(gulp.dest('./dist/js'))
 });
 
-gulp.task('minify-css', function() {
+gulp.task('minify-css', function () {
     return gulp.src('./src/css/styles.css')
         .pipe(cleanCSS({
             compatibility: 'ie8'
@@ -23,16 +23,18 @@ gulp.task('minify-css', function() {
         .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('minify-img', function() {
-    return gulp.src(['./src/img/meetup-aerolab.jpg', './src/img/logo.svg', './src/img/areatres.svg', './src/img/ekospace.svg', './src/img/aerolab.svg', './src/img/acamica.svg', './src/img/coderhouse.svg', './src/img/nodeschoolba.svg', './src/img/banode.svg'])
+gulp.task('minify-img', function () {
+    return gulp.src(['./src/img/meetup-aerolab.jpg', './src/img/logo.svg', './src/img/areatres.svg', './src/img/ekospace.svg', './src/img/aerolab.svg', './src/img/acamica.svg', './src/img/coderhouse.svg', './src/img/nodeschoolba.svg', './src/img/banode.svg', './src/img/meetupjs.svg'])
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/img'));
 });
 
-gulp.task('minify-html', function() {
-  return gulp.src('./src/html/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./'));
+gulp.task('minify-html', function () {
+    return gulp.src('./src/html/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['uglify', 'minify-css', 'minify-img','minify-html']);
+gulp.task('default', ['uglify', 'minify-css', 'minify-img', 'minify-html']);

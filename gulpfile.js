35a82e5e9f324fa19aaea3082b8dea3174fa7ce3
Leonardo Galante <lnd@MacBook-Pro-de-Leonardo.local>
2016-09-30 08:39:39 -0300

    new meetup logo, gulpfile with watch & css fixes

--- gulpfile.js
+++ gulpfile.js
@@ -7,28 +7,6 @@ var imagemin = require('gulp-imagemin');
 var htmlmin = require('gulp-htmlmin');
 
 
-gulp.task('uglify', function () {
-    return gulp.src('./src/js/script.js')
-        .pipe(uglify())
-        .pipe(rename('main.min.js'))
-        .pipe(gulp.dest('./dist/js'))
-});
-
-gulp.task('minify-css', function () {
-    return gulp.src('./src/css/styles.css')
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('./dist/css'));
-});
-
-gulp.task('minify-img', function () {
-    return gulp.src(['./src/img/meetup-aerolab.jpg', './src/img/logo.svg', './src/img/areatres.svg', './src/img/ekospace.svg', './src/img/aerolab.svg', './src/img/acamica.svg', './src/img/coderhouse.svg', './src/img/nodeschoolba.svg', './src/img/banode.svg', './src/img/meetupjs.svg'])
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/img'));
-});
-
 gulp.task('minify-html', function () {
     return gulp.src('./src/html/*.html')
         .pipe(htmlmin({
@@ -37,4 +15,30 @@ gulp.task('minify-html', function () {
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['uglify', 'minify-css', 'minify-img', 'minify-html']);
+gulp.task('minify-css', function () {
+    return gulp.src('./src/css/*.css')
+        .pipe(cleanCSS())
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('minify-js', function () {
+    return gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(rename('main.min.js'))
+        .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('minify-img', function () {
+    return gulp.src(['./src/img/*.jpg', './src/img/*.svg'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+});
+
+
+gulp.task('default', ['minify-html', 'minify-css', 'minify-js', 'minify-img', ], function () {
+    gulp.watch('./src/html/index.html', ['minify-html']);
+    gulp.watch('./src/css/styles.css', ['minify-css']);
+    gulp.watch('./src/js/script.js', ['minify-js']);
+    gulp.watch(['./src/img/*.jpg', './src/img/*.svg'], ['minify-img']);
+})

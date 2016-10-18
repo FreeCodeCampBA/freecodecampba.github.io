66c8aa73ebbba118ca535099ee28576fae67c62a
Leonardo Galante <lnd@MacBook-Pro-de-Leonardo.local>
2016-10-18 12:45:21 -0300

    Logo de Aerolab agregado en "Cuando"

--- dist/js/main.js
+++ dist/js/main.js
@@ -607,6 +607,8 @@ function setCard(num, response) {
         $(".thumbnail img").eq(num).attr("src", "./dist/img/ekospace.svg");
     } else if (response.data[num].venue.name === "Nuevo AreaTres") {
         $(".thumbnail img").eq(num).attr("src", "./dist/img/areatres.svg");
+    } else if (response.data[num].venue.name === "Aerolab") {
+        $(".thumbnail img").eq(num).attr("src", "./dist/img/aerolab.svg");
     }
 }
 
@@ -616,14 +618,14 @@ function nextMeetups() {
         url: "https://api.meetup.com/FreeCodeCampBA/events?photo-host=public&page=2&sig_id=471f4512414422381c386239397b703d",
         data: "data",
         dataType: "jsonp",
-        success: function(response) {
+        success: function (response) {
             setCard(0, response);
             setCard(1, response);
         }
     });
 }
 
-$(function() {
+$(function () {
     activeScrollpy();
     nextMeetups();
 });

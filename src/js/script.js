e58be11da3f1cf4400f54017a293dd7cf65b63d3
Fenwil <lndgalante@gmail.com>
2017-02-17 04:12:44 -0300

    Add edge cases for different Meetup API scenarios

--- src/js/script.js
+++ src/js/script.js
@@ -1,42 +1,63 @@
-function activeScrollpy () {
+function activeScrollpy() {
   $('body').scrollspy({
     target: '.navbar'
-  })
+  });
 }
 
-function capitalizeFirstLetter (string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function setCard (num, response) {
-  $('.meetup-link').eq(num).attr('href', response.data[num].link)
-  $('.caption h3').eq(num).text(response.data[num].name)
-  $('.caption h4').eq(num).text(response.data[num].venue.name)
-  $('.caption h5').eq(num).text(response.data[num].venue.address_1)
-  $('.caption h6').eq(num).text(capitalizeFirstLetter(moment(response.data[num].time).format('dddd, D MMMM, hh:mmA')))
+function setCard(num, response) {
+  $('.meetup-link').eq(num).attr('href', response.data[num].link);
+  $('.caption h3').eq(num).text(response.data[num].name);
+  $('.caption h4').eq(num).text(response.data[num].venue.name);
+  $('.caption h5').eq(num).text(response.data[num].venue.address_1);
+  $('.caption h6')
+    .eq(num)
+    .text(
+      capitalizeFirstLetter(
+        moment(response.data[num].time).format('dddd, D MMMM, hh:mmA')
+      )
+    );
   if (response.data[num].venue.name === 'EkoSpace') {
-    $('.thumbnail img').eq(num).attr('src', './dist/img/ekospace.svg')
+    $('.thumbnail img').eq(num).attr('src', './dist/img/ekospace.svg');
   } else if (response.data[num].venue.name === 'Nuevo AreaTres') {
-    $('.thumbnail img').eq(num).attr('src', './dist/img/areatres.svg')
+    $('.thumbnail img').eq(num).attr('src', './dist/img/areatres.svg');
   } else if (response.data[num].venue.name === 'Aerolab') {
-    $('.thumbnail img').eq(num).attr('src', './dist/img/aerolab.svg')
+    $('.thumbnail img').eq(num).attr('src', './dist/img/aerolab.svg');
   }
 }
 
-function nextMeetups () {
+function nextMeetups() {
   $.ajax({
     type: 'GET',
     url: 'https://api.meetup.com/FreeCodeCampBA/events?photo-host=public&page=2&sig_id=471f4512414422381c386239397b703d',
     data: 'data',
     dataType: 'jsonp',
-    success: function (response) {
-      setCard(0, response)
-      setCard(1, response)
+    success: function(response) {
+      console.log(response);
+      if (response) {
+        if (response.data.length === 1) {
+          setCard(0, response);
+          $('.when2').remove();
+          $('.when1').removeClass('col-lg-6').addClass('col-lg-12');
+        } else if (response.data.length === 2) {
+          setCard(0, response);
+          setCard(1, response);
+        }
+        return;
+      }
+      $('.when1,.when2').remove();
+      $('.no-meetups p').text('No hay futuros Meetups planeados');
+    },
+    error: function(error) {
+      console.log(error);
     }
-  })
+  });
 }
 
-$(function () {
-  activeScrollpy()
-  nextMeetups()
-})
+$(function() {
+  activeScrollpy();
+  nextMeetups();
+});

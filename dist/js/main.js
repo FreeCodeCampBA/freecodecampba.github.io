6b29b0eba4a671a14ac8bd125d119984aeb1c181
Fenwil <lndgalante@gmail.com>
2017-07-12 18:44:23 -0300

    Add Wolox logo

--- dist/js/main.js
+++ dist/js/main.js
@@ -588,34 +588,43 @@ a.version="2.15.1",b(rb),a.fn=Se,a.min=tb,a.max=ub,a.now=Fe,a.utc=j,a.unix=Jc,a.
 
 }));
 function activeScrollpy() {
-  $('body').scrollspy({
-    target: '.navbar',
-  });
+  $('body').scrollspy({ target: '.navbar' })
 }
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 function setCard(num, response) {
-  $('.meetup-link').eq(num).attr('href', response.data[num].link);
-  $('.caption h3').eq(num).text(response.data[num].name);
-  $('.caption h4').eq(num).text(response.data[num].venue.name);
-  $('.caption h5').eq(num).text(response.data[num].venue.address_1);
-  $('.caption h6').eq(num).text(capitalizeFirstLetter(moment(response.data[num].time).format('dddd, D MMMM, hh:mmA')));
-  if (response.data[num].venue.name === 'EkoSpace') {
-    $('.thumbnail img').eq(num).attr('src', './dist/img/ekospace.svg');
-  } else if (response.data[num].venue.name === 'Nuevo AreaTres') {
-    $('.thumbnail img').eq(num).attr('src', './dist/img/areatres.svg');
-  } else if (response.data[num].venue.name === 'Aerolab') {
-    $('.thumbnail img').eq(num).attr('src', './dist/img/aerolab.svg');
+  var venueName = response.data[num].venue.name
+  $('.meetup-link').eq(num).attr('href', response.data[num].link)
+  $('.caption h3').eq(num).text(response.data[num].name)
+  $('.caption h4').eq(num).text(venueName)
+  $('.caption h5').eq(num).text(response.data[num].venue.address_1)
+  $('.caption h6')
+    .eq(num)
+    .text(
+      capitalizeFirstLetter(
+        moment(response.data[num].time).format('dddd, D MMMM, hh:mmA')
+      )
+    )
+
+  if (venueName === 'EkoSpace') {
+    $('.thumbnail img').eq(num).attr('src', 'dist/img/ekospace.svg')
+  } else if (venueName === 'Nuevo AreaTres') {
+    $('.thumbnail img').eq(num).attr('src', 'dist/img/areatres.svg')
+  } else if (venueName === 'Aerolab') {
+    $('.thumbnail img').eq(num).attr('src', 'dist/img/aerolab.svg')
+  } else if (venueName === 'Wolox') {
+    $('.thumbnail img').eq(num).attr('src', 'dist/img/wolox.svg')
   }
 }
 
 function nextMeetups() {
   $.ajax({
     type: 'GET',
-    url: 'https://api.meetup.com/FreeCodeCampBA/events?photo-host=public&page=2&sig_id=471f4512414422381c386239397b703d',
+    url:
+      'https://api.meetup.com/FreeCodeCampBA/events?photo-host=public&page=2&sig_id=471f4512414422381c386239397b703d',
     data: 'data',
     dataType: 'jsonp',
     success: function(response) {
@@ -623,30 +632,30 @@ function nextMeetups() {
       if (response) {
         switch (response.data.length) {
           case 0:
-            $('.when1,.when2').remove();
-            $('.no-meetups p').text('No hay futuros Meetups planeados');
-            return;
+            $('.when1,.when2').remove()
+            $('.no-meetups p').text('No hay futuros Meetups planeados')
+            return
           case 1:
-            setCard(0, response);
-            $('.when2').remove();
-            $('.when1').removeClass('col-lg-6').addClass('col-lg-12');
-            return;
+            setCard(0, response)
+            $('.when2').remove()
+            $('.when1').removeClass('col-lg-6').addClass('col-lg-12')
+            return
           case 2:
-            setCard(0, response);
-            setCard(1, response);
-            return;
+            setCard(0, response)
+            setCard(1, response)
+            return
           default:
-            return;
+            return
         }
       }
     },
     error: function(error) {
-      console.log(error);
+      console.log(error)
     },
-  });
+  })
 }
 
 $(function() {
-  activeScrollpy();
-  nextMeetups();
-});
+  activeScrollpy()
+  nextMeetups()
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,66 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
-var imagemin = require('gulp-imagemin');
-var htmlmin = require('gulp-htmlmin');
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
+const cleanCSS = require('gulp-clean-css')
+const imagemin = require('gulp-imagemin')
+const htmlmin = require('gulp-htmlmin')
 
+gulp.task('minify-html', () =>
+  gulp
+    .src('./src/html/*.html')
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest('./'))
+)
 
-gulp.task('minify-html', function () {
-    return gulp.src('./src/html/*.html')
-        .pipe(htmlmin({
-            collapseWhitespace: true
-        }))
-        .pipe(gulp.dest('./'));
-});
+gulp.task('minify-css', () =>
+  gulp
+    .src('./src/css/*.css')
+    .pipe(cleanCSS())
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('./dist/css'))
+)
 
-gulp.task('minify-css', function () {
-    return gulp.src('./src/css/*.css')
-        .pipe(cleanCSS())
-        .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('./dist/css'));
-});
+gulp.task('concat-js', () =>
+  gulp
+    .src([
+      './src/lib/jquery.min.js',
+      './src/lib/bootstrap.min.js',
+      './src/lib/moment.min.js',
+      './src/lib/locale-es.js',
+      './src/js/script.js',
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./dist/js'))
+}
 
-gulp.task('concat-js', function () {
-    return gulp.src(['./src/lib/jquery.min.js', './src/lib/bootstrap.min.js', './src/lib/moment.min.js', './src/lib/locale-es.js', './src/js/script.js'])
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./dist/js'));
-});
+gulp.task('minify-js', () => 
+  gulp
+    .src('./dist/js/main.js')
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('./dist/js'))
+)
 
-gulp.task('minify-js', function () {
-    return gulp.src('./dist/js/main.js')
-        .pipe(uglify())
-        .pipe(rename('main.min.js'))
-        .pipe(gulp.dest('./dist/js'))
-});
+gulp.task('minify-img', () =>
+ gulp
+    .src(['./src/img/*.jpg', './src/img/*.png', './src/img/*.svg'])
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'))
+)
 
-gulp.task('minify-img', function () {
-    return gulp.src(['./src/img/*.jpg', './src/img/*.png', './src/img/*.svg'])
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/img'));
-});
-
-gulp.task('default', ['minify-html', 'minify-css', 'concat-js', 'minify-js', 'minify-img'], function () {
-    gulp.watch('./src/html/index.html', ['minify-html']);
-    gulp.watch('./src/css/styles.css', ['minify-css']);
-    gulp.watch('./src/js/script.js', ['concat-js']);
-    gulp.watch('./src/js/script.js', ['minify-js']);
-    gulp.watch(['./src/img/*.jpg', './src/img/*.svg'], ['minify-img']);
-})
+gulp.task(
+  'default',
+  ['minify-html', 'minify-css', 'concat-js', 'minify-js', 'minify-img'],
+  () => {
+    gulp.watch('./src/html/index.html', ['minify-html'])
+    gulp.watch('./src/css/styles.css', ['minify-css'])
+    gulp.watch('./src/js/script.js', ['concat-js'])
+    gulp.watch('./src/js/script.js', ['minify-js'])
+    gulp.watch(['./src/img/*.jpg', './src/img/*.svg'], ['minify-img'])
+  }
+)   

335e74a849df786e61db1b8925c49d007fccde66
lndgalante <lndgalante@gmail.com>
2019-02-25 20:28:24 -0300

    Add styled component gatsby plugin

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@freecodecampba`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
